bfc8238241e132f94955e3e43ffd53403f612a61
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-11-16 13:40:20 -0700

    mmm

--- src/App.js
+++ src/App.js
@@ -1,17 +1,69 @@
-import './App.css';
+import "./App.css";
+
 
 function App() {
+  function handleHate(){
+    let activeIndex = 0;
+    const groups = document.getElementsByClassName("card-group");
+  }
+
+  function handleLove(){
+    console.log('handle love')
+    let activeIndex = 0;
+    const groups = document.getElementsByClassName("card-group");
+    const nextIndex = activeIndex+1 <= groups.length - 1 ? activeIndex+1 : 0;
+    console.log(activeIndex,nextIndex);
+    console.log(`[data-index="${activeIndex}"]`);
+    const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+          nextGroup = document.querySelector(`[data-index="${nextIndex}]"`);
+
+    currentGroup.dataset.status = "after";
+    console.log(nextGroup);
+    nextGroup.dataset.status = "active";
+    activeIndex = nextIndex;
+  }
+
   return (
     <div className="App">
-      <div className="card-group"> 
-      <div className="little-card card"> </div>
-      <div className="big-card card"> </div>
-      <div className="little-card card"> </div>
-      <div className="big-card card"> </div>
-      <div className="little-card card"> </div>
-      <div className="big-card card"> </div>
-      <div className="little-card card"> </div>
-      <div className="big-card card"> </div>
+
+      <div className="card-swiper"> 
+      <div className="card-groups">
+        <div className="card-group" data-index="0" data-status="active">
+          <div className="little-card card"> </div>
+          <div className="big-card card"> </div>
+          <div className="little-card card"> </div>
+          <div className="big-card card"> </div>
+          <div className="little-card card"> </div>
+          <div className="big-card card"> </div>
+          <div className="little-card card"> </div>
+          <div className="big-card card"> </div>
+        </div>
+        <div className="card-group" data-index="1" data-status="unknown">
+          <div className="little-card card"> </div>
+          <div className="big-card card"> </div>
+          <div className="little-card card"> </div>
+          <div className="big-card card"> </div>
+          <div className="little-card card"> </div>
+          <div className="big-card card"> </div>
+          <div className="little-card card"> </div>
+          <div className="big-card card"> </div>
+        </div>
+        <div className="card-group" data-index="2" data-status="unknown">
+          <div className="little-card card"> </div>
+          <div className="big-card card"> </div>
+          <div className="little-card card"> </div>
+          <div className="big-card card"> </div>
+          <div className="little-card card"> </div>
+          <div className="big-card card"> </div>
+          <div className="little-card card"> </div>
+          <div className="big-card card"> </div>
+        </div>
+      </div>
+      <div className="card-swiper-buttons"> 
+        <button id="hate-button" onClick={handleHate}> hate</button>
+        <button id="love-button" onClick={handleLove}> Love</button>
+      </div>
+
       </div>
     </div>
   );
